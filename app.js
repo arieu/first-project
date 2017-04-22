decd3f0828357e06dc1593e93cf71e0d7bbf9623
fochoa <francisco.r.ochoa@gmail.com>
2017-04-21 17:02:16 -0700

    UpdatedJSON and server is config

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // main server app file
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
 const app = express();
+
+app.set('port', process.env.PORT || 3015);
+app.use(logger('dev'));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(app.get('port'), function(err) {
+	console.log(`Express Server listening on ${app.get('port')} in ${app.get('env')} mode`);
+});
